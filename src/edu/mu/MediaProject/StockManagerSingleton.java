ac50dac7e9426ff44aab17e8de3d78d084283ab6
Jackson Morgan <jake81morgan@gmail.com>
2024-02-12 19:24:40 -0600

    Update StockManagerSingleton.java
    created members and base Constructor. Also added getInstance method to make sure only one instance of the StockManagerSingleton is created

--- src/edu/mu/MediaProject/StockManagerSingleton.java
+++ src/edu/mu/MediaProject/StockManagerSingleton.java
@@ -1,5 +1,22 @@
 package edu.mu.MediaProject;
 
 public class StockManagerSingleton {
+	
+	private static final String inventoryFilePath = "inventory.csv";
+	private static StockManagerSingleton instance = null;
+	private ArrayList<MediaProduct> inventory;
+	
+	private StockManagerSingleton() {
+		
+	}
+	
+	public static StockManagerSingleton getInstance() {
+		if(instance == null) {
+			instance = new StockManagerSingleton();
+		}
+		return instance;
+		
+	}
+	
 
 }
