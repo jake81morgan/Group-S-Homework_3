0a33a016b3144f5e359e9bd14c97b7e922b83725
evanboyd <evanboyd@test-168199.dhcp.missouri.edu>
2024-02-13 12:47:24 -0600

    Added Getters and Setters for MediaProduct

--- src/edu/mu/MediaProject/MediaProducts/MediaProduct.java
+++ src/edu/mu/MediaProject/MediaProducts/MediaProduct.java
@@ -15,5 +15,38 @@ public class MediaProduct {
 		this.genre = genre;
 		
 	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+	
 	
 }
