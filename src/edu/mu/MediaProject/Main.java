5bc40bb3a2b4b449386d8885d93855de52ca369b
Jackson Morgan <jake81morgan@gmail.com>
2024-02-12 19:25:23 -0600

    created initializeStock
    made method that reads from file and adds products to inventory

--- src/edu/mu/MediaProject/Main.java
+++ src/edu/mu/MediaProject/Main.java
@@ -3,7 +3,9 @@ package edu.mu.MediaProject;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		StockManagerSingleton inventory = StockManagerSingleton.getInstance();
+		inventory.initializeStock();
 
 	}
 
--- src/edu/mu/MediaProject/StockManagerSingleton.java
+++ src/edu/mu/MediaProject/StockManagerSingleton.java
@@ -1,5 +1,10 @@
 package edu.mu.MediaProject;
 
+import java.io.*;
+import java.util.*;
+
+import edu.mu.MediaProject.MediaProducts.*;
+
 public class StockManagerSingleton {
 	
 	private static final String inventoryFilePath = "inventory.csv";
@@ -18,5 +23,78 @@ public class StockManagerSingleton {
 		
 	}
 	
+	
+	public boolean initializeStock() {
+		
+		inventory = new ArrayList<MediaProduct>();
+		
+		try {
+			
+			// Initializes the file reader
+			Scanner scanner = new Scanner(new FileInputStream(inventoryFilePath));
+			
+			// Skips header line of file
+			scanner.nextLine();
+			
+			// Reads product info from file
+			while(scanner.hasNext()) {
+				
+				MediaProduct product;
+				String[] info;
+				
+				// Read product info off file
+				info = scanner.nextLine().split(",");
 
+				String type = info[0];
+				String title = info[1];
+				double price = Double.parseDouble(info[2]);
+				int year = Integer.parseInt(info[3]);
+				Genre genre = Genre.valueOf(info[4]);
+				
+				// Add product to inventory
+				if(info[0].equals("CD")) {
+					product = new CDRecordProduct(title, price, year, genre);
+					inventory.add(product);
+				} else if(info[0].equals("Tape")) {
+					product = new TapeRecordProduct(title, price, year, genre);
+					inventory.add(product);
+					
+				} else if(info[0].equals("Vinyl")) {
+					product = new VinylRecordProduct(title, price, year, genre);
+					inventory.add(product);
+				}
+				
+			}
+			
+			return true;
+			
+		  // Thrown if file directory is incorrect
+		} catch (FileNotFoundException e) {
+			
+			System.out.println("File not found.");
+			return false;
+			
+		}
+		
+	}
+
+	
+	//public boolean updateItemPrice(MediaProduct product, double newPrice);
+	
+	//public boolean addItem(MediaProduct product);
+	
+	//public boolean removeItem(MediaProduct product);
+	
+	//public boolean saveStock();
+	
+	//public ArrayList<MediaProduct> getMediaProductBelowPrice(int maxPrice);
+	
+	//public void printListOfMediaProduct(ArrayList<MediaProduct> productList);
+	
+	//public ArrayList<VinylRecordProduct> getVinylRecordList(ArrayList<MediaProduct> productList);
+	
+	//public ArrayList<CDRecordProduct> getCDRecordsList(ArrayList<MediaProduct> productList);
+	
+	//public ArrayList<TapeRecordProduct> getTapeRecordList(ArrayList<MediaProduct> productList);
+	
 }
