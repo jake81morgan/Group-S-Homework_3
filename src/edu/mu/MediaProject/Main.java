ad74066facb4359669b97334dc7c444e2b36840c
Jackson Morgan <jake81morgan@gmail.com>
2024-02-16 22:19:12 -0600

    Update Main.java
    updated the main class to test all of the methods

--- src/edu/mu/MediaProject/Main.java
+++ src/edu/mu/MediaProject/Main.java
@@ -1,41 +1,68 @@
 package edu.mu.MediaProject;
 
-import java.util.*;
-
+import java.util.ArrayList;
+import edu.mu.MediaProject.MediaProducts.CDRecordProduct;
 import edu.mu.MediaProject.MediaProducts.Genre;
 import edu.mu.MediaProject.MediaProducts.MediaProduct;
+import edu.mu.MediaProject.MediaProducts.TapeRecordProduct;
+import edu.mu.MediaProject.MediaProducts.VinylRecordProduct;
 
 
 public class Main {
 	public static void main(String[] args) {
 		StockManagerSingleton inventory = StockManagerSingleton.getInstance();
 		
-		boolean Initialization = inventory.initializeStock();
-		//Test to see if the Initialization went through or not
-		if(!Initialization) {
-			System.out.println("Could not initialize the stock");
-			return ;
+		// Checks if initializingStock works
+		if(!inventory.initializeStock()) {
+			System.out.println("Inventory was not initialized.");
 		}
 		
-		// Retrieve the inventory
-       ArrayList<MediaProduct> Inventory = inventory.getInventory();
-       // Loop through the inventory and print each item
-       System.out.println("Inventory:");
-       for (MediaProduct product : Inventory) {
-           System.out.println(product);
-       }  
-       
-       if(inventory.saveStock()) {
-    	   System.out.println("Inventory written to CSV file.");
-       } else {
-    	   System.out.println("Writing error.");
-       }
-       
-       ArrayList<MediaProduct> belowPriceProducts = inventory.getMediaProductBelowPrice(14);
-       System.out.println("Products less than $14");
-       for (MediaProduct product : belowPriceProducts) {
-           System.out.println(product);
-       }
+		// Checks getMediaProductBelowPrice method
+		int price = 15;
+		System.out.println("***************Items below $15*****************\n");
+		for(MediaProduct product : inventory.getMediaProductBelowPrice(price)) {
+			System.out.println(product);
+		}
+		
+		System.out.println("\n***************Other Method Checks*****************\n");
+		// Checks updateItemPrice
+		MediaProduct product = inventory.getInventory().get(0);
+		if(inventory.updateItemPrice(product, 100)) {
+			System.out.println("Price Updated to 100.");
+		}
+		
+		// Checks adding and removing methods
+		product = inventory.getInventory().get(1);
+		MediaProduct product_2 = new CDRecordProduct("Tester", 20.22, 2024, Genre.POP);
+		if(inventory.removeItem(product)) {
+			System.out.println("Removal correct.");
+		}
+		if(inventory.addItem(product_2)) {
+			System.out.println("Addition correct.");
+		}
+		
+		// Checks saveStock method
+		if(inventory.saveStock()) {
+			System.out.println("Inventory updated to CSV");
+		}
+		
+		// Check getting CD, Tape, and Vinyl lists
+		ArrayList<MediaProduct> products = inventory.getInventory();
+		System.out.println("\n***************CD List*****************\n");
+		for(CDRecordProduct cd : inventory.getCDRecordsList(products)) {
+			System.out.println(cd);
+		}
+		System.out.println("\n***************Tape List*****************\n");
+		for(TapeRecordProduct tape : inventory.getTapeRecordList(products)) {
+			System.out.println(tape);
+		}
+		System.out.println("\n***************Vinyl List*****************\n");
+		for(VinylRecordProduct vinyl : inventory.getVinylRecordList(products)) {
+			System.out.println(vinyl);
+		}
+		
+		
+    
        
 	}
 }
